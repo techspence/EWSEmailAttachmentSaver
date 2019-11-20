fced154f76be1b535f8ae491dd6afbfbe352efd5
b.b.2k16 <b.b.2k16@yandex.com>
2019-11-20 10:43:30 +0000

    Generalized the script
    The saving of attachment assumes the attachments has the string date on it, as well as the string "workstation" / "server". This change added a new $attachmentfilter user defined variable for user to define what is the name of the attachment they are looking for specificity. Since the creation of the attachment destination folder is based on the name of the attachment, this approach has been changed to based on the date of the email received instead.

--- EWSEmailAttachmentSaver.ps1
+++ EWSEmailAttachmentSaver.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
   .SYNOPSIS
 	This script looks for specific emails in an exchange users mailbox, downloads the attachments, 
 	then marks those emails as read and moves the messages to a processed folder for archiving.
@@ -42,95 +42,82 @@ $logname = "EWSAttachmentSaver-$(get-date -f yyyy-MM-dd).log"
 $logfile = $logpath + $logname
 $processedfolderpath = "/Processed"
 $subjectfilter = "Patch Report"
+$attachmentfilter = "Leavers"
 $datestamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
 
-Function LogWrite
-{
-	Param ([string]$logstring)
+Function LogWrite {
+    Param ([string]$logstring)
 	
-	if (!(Test-Path $logpath)) {
-		New-Item -ItemType Directory $logpath
-	} 
-	else { 
-		Add-content $logfile -value $logstring
-	}
+    if (!(Test-Path $logpath)) {
+        New-Item -ItemType Directory $logpath
+    } 
+    else { 
+        Add-content $logfile -value $logstring
+    }
 }
 
-Function FindTargetFolder($folderpath){
-	$tftargetidroot = New-Object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot,$mailbox)
-	$tftargetfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($exchangeservice,$tftargetidroot)
-  $pfarray = $folderpath.Split("/")
+Function FindTargetFolder($folderpath) {
+    $tftargetidroot = New-Object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot, $mailbox)
+    $tftargetfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($exchangeservice, $tftargetidroot)
+    $pfarray = $folderpath.Split("/")
 	
-	# Loop processed folders path until target folder is found
-	for ($i = 1; $i -lt $pfarray.Length; $i++){
-		$fvfolderview = New-Object Microsoft.Exchange.WebServices.Data.FolderView(1)
-		$sfsearchfilter = New-Object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.FolderSchema]::DisplayName,$pfarray[$i])
-    $findfolderresults = $exchangeservice.FindFolders($tftargetfolder.Id,$sfsearchfilter,$fvfolderview)
+    # Loop processed folders path until target folder is found
+    for ($i = 1; $i -lt $pfarray.Length; $i++) {
+        $fvfolderview = New-Object Microsoft.Exchange.WebServices.Data.FolderView(1)
+        $sfsearchfilter = New-Object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.FolderSchema]::DisplayName, $pfarray[$i])
+        $findfolderresults = $exchangeservice.FindFolders($tftargetfolder.Id, $sfsearchfilter, $fvfolderview)
 		
-		if ($findfolderresults.TotalCount -gt 0){
-			foreach ($folder in $findfolderresults.Folders){
-				$tftargetfolder = $folder				
-			}
-		}
-		else {
-			LogWrite "### Error ###"
-			Logwrite $datestamp " : Folder Not Found"
-			$tftargetfolder = $null
-			break
-		}	
-	}
-	$Global:findFolder = $tfTargetFolder
+        if ($findfolderresults.TotalCount -gt 0) {
+            foreach ($folder in $findfolderresults.Folders) {
+                $tftargetfolder = $folder				
+            }
+        }
+        else {
+            LogWrite "### Error ###"
+            Logwrite $datestamp " : Folder Not Found"
+            $tftargetfolder = $null
+            break
+        }	
+    }
+    $Global:findFolder = $tfTargetFolder
 }
 
-Function FindTargetEmail($subject){
-	foreach ($email in $foundemails.Items){
-		$email.Load()
-		$attachments = $email.Attachments
+Function FindTargetEmail($subject) {
+    foreach ($email in $foundemails.Items) {
+        $email.Load()
+        $attachments = $email.Attachments
 
-		foreach ($attachment in $attachments){
-			$attachment.Load()
-			$attachmentname = $attachment.Name.ToString()
+        foreach ($attachment in $attachments) {
+            $attachment.Load()
+            $attachmentname = $attachment.Name.ToString()
 
-			# Find the year based on the attachment name
-			$fileyear = $attachment.Name.Split("-")[0]
-			$fileyear = $fileyear.Split("_")[1]
+            # Find the year based on the email recieved date
+            $emailDateTimeReceived = (Get-Date $email.DateTimeReceived -Format yyyyMMdd)
+            $emailYear = $emailDateTimeReceived.Substring(0, 4)
+            $emailMonth = $emailDateTimeReceived.Substring(4, 2)
+            $emailDate = $emailDateTimeReceived.Substring(6, 2)
 			
-			# Instead of (01, 02, 03, etc.) I want (1, 2, 3, etc.) for monthly folder names
-			# Why? Because... :D
-			$filemonth = $attachment.Name.Split("-")[1]
-			if ($filemonth -eq "10"){}
-			elseif ($filemonth -gt "10"){} 
-			else {$filemonth = $filemonth.Split("0")[1]}
-
-			if ($attachmentname -like '*Workstation*'){
-				$workstationreportfolder = $reportroot + "Workstations\" + $fileyear + "\" + $filemonth + "\"
-				if (!(Test-Path $workstationreportfolder)) {
-					LogWrite "$workstationreportfolder not found.."
-					LogWrite "Creating it now.."
-					New-Item -ItemType Directory $workstationreportfolder | Out-Null
-				}
-				LogWrite "$attachmentname saved to $workstationreportfolder"
-				$file = New-Object System.IO.FileStream(($workstationreportfolder + $attachmentname), [System.IO.FileMode]::Create)
-				$file.Write($attachment.Content, 0, $attachment.Content.Length)
-				$file.Close()
-			} elseif ($attachmentname -like '*Server*'){
-				$serverreportfolder = $reportroot + "Servers\" + $fileyear + "\" + $filemonth + "\"
-				if (!(Test-Path $serverreportfolder)) {
-					LogWrite "$serverreportfolder not found.."
-					LogWrite "Creating it now.."
-					New-Item -ItemType Directory $serverreportfolder | Out-Null
-				}
-				LogWrite "$attachmentname saved to $serverreportfolder"	
-				$file = New-Object System.IO.FileStream(($serverreportfolder + $attachmentname), [System.IO.FileMode]::Create)	
-				$file.Write($attachment.Content, 0, $attachment.Content.Length)
-				$file.Close()
-			} else {}
-		}
-	# Mark email as read & move to processed folder
-	$email.IsRead = $true
-	$email.Update([Microsoft.Exchange.WebServices.Data.ConflictResolutionMode]::AlwaysOverwrite)
-	[VOID]$email.Move($Global:findFolder.Id)
-	}
+            if ($attachmentname -like "*$($attachmentfilter)*") {   
+                $attachmentdownloadfolderpath = $reportroot + "Attachments\" + $emailyear + "\" + $emailmonth + "\" + $emailDate + "\"
+                if (!(Test-Path $attachmentdownloadfolderpath)) {
+                    LogWrite "$attachmentdownloadfolderpath not found.."
+                    LogWrite "Creating it now.."
+                    New-Item -ItemType Directory $attachmentdownloadfolderpath | Out-Null
+                }
+                LogWrite "$attachmentname saved to $attachmentdownloadfolderpath"
+                $file = New-Object System.IO.FileStream(($attachmentdownloadfolderpath + $attachmentname), [System.IO.FileMode]::Create)
+                $file.Write($attachment.Content, 0, $attachment.Content.Length)                
+                $file.Close()
+            }
+            else {
+                LogWrite "No attachment found"
+            }
+        }
+        # Mark email as read & move to processed folder
+        $email.IsRead = $true
+        $email.Update([Microsoft.Exchange.WebServices.Data.ConflictResolutionMode]::AlwaysOverwrite)
+        [VOID]$email.Move($Global:findFolder.Id)
+    }
 }
 
 LogWrite "DATETIME: $datestamp"
@@ -151,8 +138,8 @@ $exchangeservice.UseDefaultCredentials = $true
 $exchangeservice.AutodiscoverUrl($mailbox)
 
 # Bind to the Inbox folder of the target mailbox
-$inboxfolderid = New-Object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::Inbox,$mailbox)
-$inboxfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($exchangeservice,$inboxfolderid)
+$inboxfolderid = New-Object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::Inbox, $mailbox)
+$inboxfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($exchangeservice, $inboxfolderid)
 
 # Search the Inbox for messages that are: unread, has specific subject AND has attachment(s)
 $sfunread = New-Object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.EmailMessageSchema]::IsRead, $false)
@@ -165,7 +152,7 @@ $sfcollection.add($sfattachment)
 
 # Use -ArgumentList 10 to reduce query overhead by viewing the Inbox 10 items at a time
 $view = New-Object -TypeName Microsoft.Exchange.WebServices.Data.ItemView -ArgumentList 10
-$foundemails = $inboxfolder.FindItems($sfcollection,$view)
+$foundemails = $inboxfolder.FindItems($sfcollection, $view)
 
 # Find $processedfolderpath Folder ID
 FindTargetFolder($processedfolderpath)
